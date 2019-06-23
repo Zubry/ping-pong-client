28aa565a104c2ea5ead4caa62db0ca50d086f66e
Zubry <zubryjs@gmail.com>
2019-06-23 17:23:09 -0400

    Make responsive

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -5,4 +5,13 @@ export default styled.form`
   display: flex;
   align-items: flex-end;
   justify-content: space-around;
+  flex-wrap: wrap;
+
+  @media (max-width: 599px) {
+    &>div {
+      flex-basis: 100%;
+      text-align: center;
+      margin: 2em 0;
+    }
+  }
 `;
