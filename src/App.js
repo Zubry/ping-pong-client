a48d02460b057b5991203611a6d305bf9d1a3ff0
Zubry <zubryjs@gmail.com>
2019-06-23 16:51:48 -0400

    Create input component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Text from './components/text';
 
 function App() {
   return (
     <div className="App">
+      <Text/>
     </div>
   );
 }
---
+++ src/components/text.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Input = styled.input`
+  font-size: 18px;
+  padding: 0.5rem;
+  border: 0;
+  position: relative;
+
+  &:focus {
+    outline: none;
+  }
+`
+
+const InputWrapper = styled.div`
+  position: relative;
+  display: inline-block;
+
+  &:before {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    height: 3px;
+    width: 100%;
+    background-color: transparent;
+    z-index: 10;
+  }
+
+  input:focus ~ span {
+    width: 100%;
+    transition: 0.2s cubic-bezier(.5, -0.5, .5, 1);
+  }
+`
+
+const InputBorder = styled.span`
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  height: 3px;
+  background-color: #75434D;
+  z-index: 20;
+  transform: translate(-50%, 0);
+  transition: 0.2s cubic-bezier(.5, -0.5, .5, 1);
+  width: 0%;
+`;
+
+export default () => (
+  <InputWrapper>
+    <Input type="text" />
+    <InputBorder />
+  </InputWrapper>
+)
